ec9e60b41bb17be2da229bb4e4587a86da09b8d4
Humberto Leal <humberto.adolfo@hotmail.com>
2020-02-08 16:05:30 -0500

    Disabling GPS consumption by putting it to sleep

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,6 @@
 
 #include "VariableType/VariableType.h"
 #include "SoilMoisture/SoilMoisture.h"
-
-#define LMIC_ENABLE_event_logging 1
 // For storing data on non-volatile memory
 
 struct Sensor
@@ -60,66 +58,54 @@ const lmic_pinmap lmic_pins = {
     .dio = {26, 33, 32},
 };
 
+void enable_sleep()
+{ //TODO implement  UBX-ACK
+    do
+    { //We cannot read UBX ack therefore try to sleep gps until it does not send data anymore
+        Serial.println("try to sleep gps!");
+        byte CFG_RST[12] = {0xb5, 0x62, 0x06, 0x04, 0x04, 0x00, 0x00, 0x00, 0x01, 0x00, 0x0F, 0x66};
+        delay(600);                                                                                                                  //give some time to restart //TODO wait for ack
+        const byte RXM_PMREQ[16] = {0xb5, 0x62, 0x02, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x4d, 0x3b}; //power off until wakeup call
+        Serial1.write(RXM_PMREQ, sizeof(RXM_PMREQ));
+        unsigned long startTime = millis();
+        unsigned long offTime = 1;
+        Serial.println(offTime);
+
+        while (millis() - startTime < 1000)
+        { //wait for the last command to finish
+            int c = Serial1.read();
+            if (offTime == 1 && c == -1)
+            { //check  if empty
+                offTime = millis();
+            }
+            else if (c != -1)
+            {
+                offTime = 1;
+            }
+            if (offTime != 1 && millis() - offTime > 100)
+            { //if gps chip does not send any commands for .. seconds it is sleeping
+                Serial.println("sleeping gps!");
+                return;
+            }
+        }
+    } while (1);
+}
+
 void onEvent(ev_t ev)
 {
     switch (ev)
     {
-    case EV_SCAN_TIMEOUT:
-        Serial.println(F("EV_SCAN_TIMEOUT"));
-        break;
-    case EV_BEACON_FOUND:
-        Serial.println(F("EV_BEACON_FOUND"));
-        break;
-    case EV_BEACON_MISSED:
-        Serial.println(F("EV_BEACON_MISSED"));
-        break;
-    case EV_BEACON_TRACKED:
-        Serial.println(F("EV_BEACON_TRACKED"));
-        break;
-    case EV_JOINING:
-        Serial.println(F("EV_JOINING"));
-        break;
-    case EV_JOINED:
-        Serial.println(F("EV_JOINED"));
-        // Disable link check validation (automatically enabled
-        // during join, but not supported by TTN at this time).
-        LMIC_setLinkCheckMode(0);
-        break;
-    case EV_RFU1:
-        Serial.println(F("EV_RFU1"));
-        break;
-    case EV_JOIN_FAILED:
-        Serial.println(F("EV_JOIN_FAILED"));
-        break;
-    case EV_REJOIN_FAILED:
-        Serial.println(F("EV_REJOIN_FAILED"));
-        break;
+
     case EV_TXCOMPLETE:
         Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
         digitalWrite(BUILTIN_LED, LOW);
         // Schedule next transmission
         esp_sleep_enable_timer_wakeup(TX_INTERVAL * 1000000);
-
         esp_deep_sleep_start();
+
         //ESP.restart();
         //delay(TX_INTERVAL * 1000);
         do_send(&sendjob);
-        break;
-    case EV_LOST_TSYNC:
-        Serial.println(F("EV_LOST_TSYNC"));
-        break;
-    case EV_RESET:
-        Serial.println(F("EV_RESET"));
-        break;
-    case EV_RXCOMPLETE:
-        // data received in ping slot
-        Serial.println(F("EV_RXCOMPLETE"));
-        break;
-    case EV_LINK_DEAD:
-        Serial.println(F("EV_LINK_DEAD"));
-        break;
-    case EV_LINK_ALIVE:
-        Serial.println(F("EV_LINK_ALIVE"));
         break;
     default:
         Serial.println(F("Unknown event"));
@@ -136,16 +122,8 @@ void do_send(osjob_t *j)
     int8_t soil_moisture_value = 0;
     uint8_t id_sensor = 0;
 
-    if (current_variable == 0)
-    {
-        soil_moisture_adc = soil_m1_sensor.get_soil_moisture_reading();
-        id_sensor = soil_m1.id_sensor;
-    }
-    else if (current_variable == 1)
-    {
-        soil_moisture_adc = soil_m2_sensor.get_soil_moisture_reading();
-        id_sensor = soil_m2.id_sensor;
-    }
+    soil_moisture_adc = soil_m1_sensor.get_soil_moisture_reading();
+    id_sensor = soil_m1.id_sensor;
 
     // Serial.print("Raw: ");
     // Serial.println(soil_moisture_adc);
@@ -154,21 +132,21 @@ void do_send(osjob_t *j)
     // Serial.println(soil_moisture_value);
     soil_moisture_value = (soil_moisture_value < 0) ? 0 : soil_moisture_value;
 
-    // Message msg = msg_manager.createSoilMoistureMessage(id_sensor, SOIL_MOISTURE, soil_moisture_value);
-    // uint8_t *buffer = msg.getMessageArray();
-    // int buffer_size = msg.getMessageArraySize();
+    Message msg = msg_manager.createSoilMoistureMessage(id_sensor, SOIL_MOISTURE, soil_moisture_value);
+    uint8_t *buffer = msg.getMessageArray();
+    int buffer_size = msg.getMessageArraySize();
 
-    uint8_t buffer[] = {id_sensor, SOIL_MOISTURE, soil_moisture_value};
+    // uint8_t buffer[] = {id_sensor, SOIL_MOISTURE, soil_moisture_value};
 
-    current_variable++;
+    // current_variable++;
 
-    if (current_variable >= number_of_variables)
-        current_variable = 0;
+    // if (current_variable >= number_of_variables)
+    //     current_variable = 0;
 
     LMIC_setTxData2(1, buffer, sizeof(buffer), 0);
     Serial.println(F("Packet queued"));
     // Serial.println("Borrado");
-    // delete[] buffer;
+    delete[] buffer;
     // digitalWrite(BUILTIN_LED, HIGH);
 
     storage_manager.putUInt("sensor_index", current_variable);
@@ -187,6 +165,10 @@ void setup()
     //Turn off WiFi and Bluetooth
     WiFi.mode(WIFI_OFF);
     btStop();
+    
+    // Puts GPS to sleep.
+    Serial1.begin(9600, SERIAL_8N1, 12, 15);
+    enable_sleep();
 
     // LMIC init
     os_init();
@@ -194,16 +176,6 @@ void setup()
     LMIC_reset();
 
     LMIC_setSession(0x1, DEVADDR, NWKSKEY, APPSKEY);
-
-    // LMIC_setupChannel(0, 903900000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-    // LMIC_setupChannel(1, 904100000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI); // g-band
-    // LMIC_setupChannel(2, 904300000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-    // LMIC_setupChannel(3, 904500000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-    // LMIC_setupChannel(4, 904700000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-    // LMIC_setupChannel(5, 904900000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-    // LMIC_setupChannel(6, 905100000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-    // LMIC_setupChannel(7, 905300000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-    // LMIC_setupChannel(8, 904600000, DR_RANGE_MAP(DR_FSK, DR_FSK), BAND_MILLI);   // g2-band
 
     LMIC_selectSubBand(1);
 
@@ -214,7 +186,7 @@ void setup()
     LMIC.dn2Dr = DR_SF9;
 
     // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
-    LMIC_setDrTxpow(DR_SF10, 14);
+    LMIC_setDrTxpow(DR_SF7, 14);
 
     // It should go before do_send
     storage_manager.begin("variables_state", false);
